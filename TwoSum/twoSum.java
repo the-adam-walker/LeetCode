39b5a1e966fe5058df1ff8fb8ff04bd71ef2a76e
the-adam-walker <adamwalker2021@gmail.com>
2019-11-21 20:53:23 -0700

    Commented on complexity

--- TwoSum/twoSum.java
+++ TwoSum/twoSum.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+      // O(n^2)
       for(int firstPass = 0; firstPass < nums.length; firstPass++) {
         for(int secondPass = firstPass + 1; secondPass <  nums.length; secondPass++) {
           if(nums[firstPass] + nums[secondPass] == target) {
